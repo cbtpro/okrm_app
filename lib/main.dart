8e93af0d898d98b7f428fa652d8740576cc4e8e2
chenbitao <cbtpro@gmail.com>
2025-07-23 17:46:33 +0800

    feat: update app name

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      title: 'Flutter Demo',
+      title: "生而不庸",
       theme: const CupertinoThemeData(
         // This is the theme of your application.
         //
